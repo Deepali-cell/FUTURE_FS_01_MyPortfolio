ad888cf3f8f4e5df43a1eded81298ba646546f6a
Deepali <joysingal247@gmail.com>
2026-01-23 13:47:34 +0530

    Update projects.js

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,11 +1,4 @@
 export const projects = [
-  {
-     title: "Streaming Platform",
-    description: "Role-Based Access , Proper authentication with protected routes , used cloudinary for storing videos.",
-    github: "https://github.com/Deepali-cell/streamingVideoPlatform",
-    demo: "https://streamingvideoplatformfrontend.onrender.com/",
-    image: "/projects/streaming.jpg",
-  }, 
     {
      title: "Task Manager",
     description: "A simple and efficient task manager web application that helps users create, manage, and track their daily tasks.",
@@ -13,28 +6,7 @@ export const projects = [
     demo: "https://task-manager-umber-six.vercel.app/",
     image: "/projects/task_management.png",
   }, 
-  {
-    title: "Doctor Appointment Website",
-    description: "Admin panel, doctors, appointments. Real-time updates .",
-    github: "https://github.com/Deepali-cell/prescripto_web/tree/main",
-    demo: "https://prescripto-web-frontend.onrender.com",
-    image: "/projects/doctor.jpg",
-  },
-  // {
-  //   title: "Real-Time Chat Website",
-  //   description: "Chat with real-time messaging functionality.",
-  //   github: "https://github.com/Deepali-cell/chat-web/tree/main",
-  //   demo: "https://chat-web-frontend-sc0b.onrender.com",
-  //   image: "/projects/chat.jpg",
-  // },
-  {
-    title: "E-commerce Website",
-    description: "Simple online store with product management and checkout.",
-    github: "https://github.com/Deepali-cell/eco-web",
-    demo: "https://eco-web-qu5s.onrender.com/",
-    image: "/projects/eco.jpg",
-  },
-  {
+   {
     title: "Job Portal Website",
     description: "Post jobs, apply for jobs, and manage applications.",
     github: "https://github.com/Deepali-cell/job-portal",
@@ -49,6 +21,35 @@ export const projects = [
     demo: "https://deepali-lms-qgqc.vercel.app/",
     image: "/projects/lms.png",
   },
+  // {
+  //   title: "Real-Time Chat Website",
+  //   description: "Chat with real-time messaging functionality.",
+  //   github: "https://github.com/Deepali-cell/chat-web/tree/main",
+  //   demo: "https://chat-web-frontend-sc0b.onrender.com",
+  //   image: "/projects/chat.jpg",
+  // },
+  {
+     title: "Streaming Platform",
+    description: "Role-Based Access , Proper authentication with protected routes , used cloudinary for storing videos.",
+    github: "https://github.com/Deepali-cell/streamingVideoPlatform",
+    demo: "https://streamingvideoplatformfrontend.onrender.com/",
+    image: "/projects/streaming.jpg",
+  }, 
+  {
+    title: "E-commerce Website",
+    description: "Simple online store with product management and checkout.",
+    github: "https://github.com/Deepali-cell/eco-web",
+    demo: "https://eco-web-qu5s.onrender.com/",
+    image: "/projects/eco.jpg",
+  },
+  {
+    title: "Doctor Appointment Website",
+    description: "Admin panel, doctors, appointments. Real-time updates .",
+    github: "https://github.com/Deepali-cell/prescripto_web/tree/main",
+    demo: "https://prescripto-web-frontend.onrender.com",
+    image: "/projects/doctor.jpg",
+  },
+ 
   {
     title: "Blog Website",
     description:
@@ -57,6 +58,14 @@ export const projects = [
     github: "https://github.com/Deepali-cell/full-stack-blog-web",
     demo: "https://blog-web-seven-brown.vercel.app/",
     image: "/projects/blog.jpg",
+  },
+  {
+    title: "QuizMaker",
+    description:
+      "QuizMaker is a web app where users can take quizzes and creators can manage quizzes (CRUD). Real-time scoring tracks user performance. Built with MERN stack.",
+    github: "https://github.com/Deepali-cell/CODSOFT--QUIZMAKER",
+    demo: "https://codsoft-quizmaker-frontend.onrender.com",
+    image: "/projects/quizmaker.jpg",
   },
   {
     title: "Movie Booking Website",
@@ -73,12 +82,4 @@ export const projects = [
   //   demo: "https://zomato2-0-frontend.onrender.com/",
   //   image: "/projects/zomatoreel.jpg",
   // },
-  {
-    title: "QuizMaker",
-    description:
-      "QuizMaker is a web app where users can take quizzes and creators can manage quizzes (CRUD). Real-time scoring tracks user performance. Built with MERN stack.",
-    github: "https://github.com/Deepali-cell/CODSOFT--QUIZMAKER",
-    demo: "https://codsoft-quizmaker-frontend.onrender.com",
-    image: "/projects/quizmaker.jpg",
-  },
 ];
